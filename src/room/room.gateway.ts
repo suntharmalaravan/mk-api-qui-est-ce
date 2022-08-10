40a2a647ce1aa51b401386af008bfb2a5c73c75e
suntharm <malaravan.sunthar@grenoble-inp.org>
2022-08-10 13:45:21 +0200

    add join a room and remove a room

--- src/room/room.gateway.ts
+++ src/room/room.gateway.ts
@@ -11,20 +11,33 @@ export class RoomGateway {
   @WebSocketServer() wss: Server;
   constructor(private readonly roomService: RoomService) {}
   @SubscribeMessage('create')
-  createRoom(socket: Socket, data: any) {
-    console.log(data);
-    socket.join(data.roomName);
-    const newRoom = this.roomService.create({
-      name: data.roomName,
+  async createRoom(socket: Socket, data: any) {
+    socket.join(data.name);
+    const newRoom = await this.roomService.create({
+      name: data.name,
       status: 'open',
       hostPlayerId: data.userId,
       guestPlayerId: 0,
       hostCharacterId: data.characterId,
       guestCharacterId: 0,
     });
-    console.log(newRoom);
-    socket.to(data.roomName).emit('roomCreated', { room: data.roomName });
-    console.log('created');
-    return { event: 'roomCreated', room: data.roomName };
+    socket.to(data.name).emit('roomCreated', { room: data.name });
+    return { event: 'roomCreated', room: data.name };
+  }
+
+  @SubscribeMessage('join')
+  async joinRoom(socket: Socket, data: any) {
+    socket.join(data.name);
+    const updatedRoom = await this.roomService.addGuest(data.id, {
+      guestPlayerId: data.userId,
+      guestCharacterId: data.characterId,
+    });
+    socket.to(data.name).emit('guest joined');
+  }
+
+  @SubscribeMessage('quit')
+  async quitRoom(socket: Socket, data: any) {
+    await this.roomService.remove(data.name);
+    socket.disconnect();
   }
 }
--- src/room/room.service.ts
+++ src/room/room.service.ts
@@ -1,8 +1,13 @@
-import { Inject, Injectable } from '@nestjs/common';
+import {
+  Inject,
+  Injectable,
+  NotAcceptableException,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateRoomDto } from './dto/create-room.dto';
-import { Room as RoomEntity } from './entities/room.entity';
+import { Room, Room as RoomEntity } from './entities/room.entity';
 @Injectable()
 export class RoomService {
   constructor(
@@ -13,5 +18,27 @@ export class RoomService {
   create(createRoomDto: CreateRoomDto) {
     const newRoom = this.roomRepository.create(createRoomDto);
     return this.roomRepository.save(newRoom);
+  }
+
+  async remove(id: number): Promise<void> {
+    await this.roomRepository.delete(id);
+  }
+
+  async addGuest(id: number, roomUpdates: any): Promise<Room> {
+    const room = await this.roomRepository.findOne({
+      select: { id: true, guestPlayerId: true, guestCharacterId: true },
+      where: { id },
+    });
+    if (!room) {
+      throw new NotFoundException('Room is not found');
+    }
+    if (room.status == 'closed') {
+      throw new NotAcceptableException('Room is already closed');
+    }
+    room.guestPlayerId = roomUpdates.guestPlayerId;
+    room.guestCharacterId = roomUpdates.guestCharacterId;
+    room.status = 'closed';
+    await this.roomRepository.update(id, room);
+    return room;
   }
 }
