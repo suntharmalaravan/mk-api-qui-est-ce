4ea558f44b8570b68831ce7a24b75a1fcd4a8f36
msunthar <suntharmalaravan@gmail.com>
2025-09-27 21:24:25 +0200

    ajouter config service

--- src/auth/auth.strategy.ts
+++ src/auth/auth.strategy.ts
@@ -1,15 +1,19 @@
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { UserService } from '../user/user.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(private userService: UserService) {
+  constructor(
+    private userService: UserService,
+    private configService: ConfigService,
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: process.env.SECRET,
+      secretOrKey: configService.get<string>('SECRET'),
     });
   }
 
--- src/config/jwtAsyncConfig.config.ts
+++ src/config/jwtAsyncConfig.config.ts
@@ -4,7 +4,12 @@ import { JwtModuleAsyncOptions, JwtModuleOptions } from '@nestjs/jwt';
 export const jwtAsyncOptions: JwtModuleAsyncOptions = {
   imports: [ConfigModule],
   inject: [ConfigService],
-  useFactory: async () => ({
-    secret: process.env.SECRET,
+  useFactory: async (
+    configService: ConfigService,
+  ): Promise<JwtModuleOptions> => ({
+    secret: configService.get<string>('SECRET'),
+    signOptions: {
+      expiresIn: '24h', // Token expire après 24h
+    },
   }),
 };
